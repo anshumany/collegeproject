3ed67d63f438e5a126e6c4f63dcd6e636f0db95e
Anagh Kumar <anagh.kumar@aurigait.com>
2021-11-20 15:41:31 +0530

    Backend Routes Updated

--- Backend/mySqlNode/database/conn.js
+++ Backend/mySqlNode/database/conn.js
@@ -4,7 +4,7 @@ const mysqlConnection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
-  database: "teamproject",
+  database: "employee",
 });
 
 mysqlConnection.connect((err) => {
